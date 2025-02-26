06c7644d90a81b693526947fa919876911840ca7
Jonathan Schneider <jkschneider@gmail.com>
2025-02-26 07:49:37 -0500

    Jackson 2.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,12 @@ description = "JSON-RPC 2.0 client and server library"
 
 dependencies {
     // https://msgpack.org/
-    implementation("org.msgpack:jackson-dataformat-msgpack:latest.release")
+    // depends on Jackson 2.18 right now, so we'll introduce when we
+    // rev Jackson elsewhere
+//    implementation("org.msgpack:jackson-dataformat-msgpack:latest.release")
 
     compileOnly("io.micrometer:micrometer-core:latest.release")
-    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:latest.release")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.17.+")
 
     implementation("org.openrewrite:rewrite-properties:latest.release")
     implementation("org.openrewrite:rewrite-json:latest.release")
