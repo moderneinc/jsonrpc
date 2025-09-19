dc4300f0e75ac494c3e8a78145236f5b0b283685
Peter Streef <p.streef@gmail.com>
2025-09-19 11:23:41 +0200

    Update jackson-module-parameter-names version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     compileOnly("io.micrometer:micrometer-core:latest.release")
-    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.17.+")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.17.2")
     testImplementation("org.openrewrite:rewrite-test:latest.release")
 }
 
